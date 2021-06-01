36b430c27fdf63975a797e9b725fb721eb9fff0c
Sebastian <sebastian.sloniec@gmail.com>
2021-06-01 20:26:05 +0200

    Create jumbotron - main page and add images

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Container, Inner, Pane, Title, SubTitle, Image } from './styles/jumbotron';
+
+export default function Jumbotron({ children, direction = 'row', ...otherProps }) {
+  return (
+    <Inner direction={direction}>
+      { children }
+    </Inner>
+  )
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...otherProps }) {
+  return <Container {...otherProps}>{children}</Container>
+}
+
+Jumbotron.Pane = function JumbotronPane({ children, ...otherProps }) {
+  return <Pane {...otherProps}>{children}</Pane>
+}
+
+Jumbotron.Title = function JumbotronTitle({ children, ...otherProps }) {
+  return <Title {...otherProps}>{children}</Title>
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...otherProps }) {
+  return <SubTitle {...otherProps}>{children}</SubTitle>
+}
+
+Jumbotron.Image = function JumbotronImage({ ...otherProps }) {
+  return <Image {...otherProps} />
+}
