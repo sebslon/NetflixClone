d3d6e8b5e793afc1aec9180786fe3e57b8e14221
Sebastian <sebastian.sloniec@gmail.com>
2021-06-03 21:34:15 +0200

    Add children section to jumbotron

--- src/global-styles.js
+++ src/global-styles.js
@@ -6,5 +6,7 @@ export const GlobalStyles = createGlobalStyle`
     background-color: black;
     color: #333333;
     font-size: 16px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 `;
