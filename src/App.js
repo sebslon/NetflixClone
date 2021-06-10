8fc2d417ccdcebe5ac1ab6abc6596ef617e8953c
Sebastian <sebastian.sloniec@gmail.com>
2021-06-10 15:32:32 +0200

    Add switch to app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
-import * as ROUTES from './constants/routes';
-import { useAuth } from './hooks';
-import { Home, Browse, SignIn, SignUp } from './pages';
-import { IsUserRedirect, ProtectedRoute } from './helpers/protecting-routes';
+import * as ROUTES from "./constants/routes";
+import { useAuth } from "./hooks";
+import { Home, Browse, SignIn, SignUp } from "./pages";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/protecting-routes";
 
 export default function App() {
   const { user } = useAuth();
   
   return (
     <Router>
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
-        <SignUp />
-      </IsUserRedirect>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <SignIn />
+        </IsUserRedirect>
 
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
-        <SignIn />
-      </IsUserRedirect>
-
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
-        <Home />
-      </IsUserRedirect>
-
-      <ProtectedRoute user={user} path={ROUTES.BROWSE}>
-        <Browse />
-      </ProtectedRoute>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <SignUp />
+        </IsUserRedirect>
+        
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
